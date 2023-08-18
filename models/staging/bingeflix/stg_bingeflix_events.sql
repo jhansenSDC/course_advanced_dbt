b66b6236f52cd88487209542a5b5280db8d9df4d
Justin Hansen <justin.hansen@smiledirectclub.com>
2023-08-18 04:47:21 +0000

    change stg_bingeflix_events to incremental

--- models/staging/bingeflix/stg_bingeflix_events.sql
+++ models/staging/bingeflix/stg_bingeflix_events.sql
@@ -1,6 +1,30 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='event_id'
+    )
+}}
+
+
 WITH source AS (
 
-    SELECT * FROM {{ source('bingeflix', 'events') }}
+    SELECT
+
+        session_id,
+        created_at,
+        user_id,
+        event_name,
+        event_id,
+        {{ dbt_utils.generate_surrogate_key(['event_id']) }} AS surrogate_key
+
+    FROM {{ source('bingeflix', 'events') }}
+
+
+    {% if is_incremental() %}
+
+        WHERE created_at > (SELECT DATEADD('day', -5, MAX(created_at)) FROM {{ this }})
+
+    {% endif %}
 
 ),
 
